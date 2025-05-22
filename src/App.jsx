58136018cb2d5eca47169207c049bf0f9337edc3
MOhammad Aftab Ahmad <mohammadaftabahmad56@gmail.com>
2025-05-22 16:11:08 +0530

    Day 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <div className='bg-red-50'>App</div> 
+    </>
+  )
+}
+
+export default App
